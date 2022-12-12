288115316c5499ec289d95e702fc117a1be4debc
DJMIKE20 <m.zimm20@gmail.com>
2022-12-12 13:44:25 -0500

    refactor to use react-redux for state and dispatch

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
 
-import { useStoreContext } from '../../utils/GlobalState';
+
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
+import { useDispatch, useSelector } from "react-redux";
 
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
 
   const { cart } = state;
 
